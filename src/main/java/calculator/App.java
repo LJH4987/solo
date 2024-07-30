44ee2dc087538835024c3da99918826e83a6a4bd
Robbie <robbie331@>
2024-07-30 23:39:51 +0900

    Level 2-7 "Calculator 클래스에 원의 넓이를 반환해주는 메서드를 구현"

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,61 +7,135 @@
  * 임포트한 calculator.Calculator를 통해 calculate 클래스를 사용해 계산을 하고 결과를 출력합니다.
  */
 
+package calculator; // calculator 패키지
+/*
+ * import calculator.Calculator;를 삭제하고 package calculator; 를 추가하여 Calculator 클래스를 사용할 수 있도록 변경
+ * package calculator를 해석하면 calculator 패키지 사용을 의미
+ * 패키지란 클래스 파일을 담는 디렉토리로 클래스 파일을 구분하기 위해 사용합니다.
+ * 패키지를 사용하면 클래스 파일을 쉽게 찾을 수 있고, 클래스 파일의 중복을 피할 수 있습니다.
+ * 이런 방식으로 같은 패키지 내에 있는 클래스는 import 없이도 사용이 가능하고
+ * 다른 패키지에 있는 클래스를 사용하려면 import를 사용하여 가져와야 합니다.
+ */
+
 import java.util.Scanner; // 스캐너를 사용하기 위한 임포트
 import java.util.ArrayList; // ArrayList를 사용하기 위해 임포트
-import calculator.Calculator; // Calculator 클래스를 사용하기 위해 임포트
 
 public class App {
     public static void main(String[] args) { // 메인이 될 메서드
+        /*
+         * public static void main(String[] args) { -> 프로그램 실행을 위해 반드시 필요한 메서드로 메인 메서드라고 합니다.
+         * 메인 메서드는 프로그램을 실행할 때 가장 먼저 실행되는 메서드입니다
+         * 메인 메서드는 public static void main(String[] args) { } 형태로 작성되어야 합니다.
+         * main은 메서드 이름이며, String[] args는 문자열 배열 타입의 매개변수입니다.
+         * string을 사용하는 이유는 문자열을 사용하기 위함입니다.
+         * args는 argument의 약자로 매개변수를 의미합니다.
+         * 매개변수는 메서드를 호출할 때 전달하는 값으로, 전달된 값은 메서드 내부에서 사용할 수 있습니다.
+         */
 
         Calculator calculator = new Calculator(); // 해석하면  Calculator calculator = new Calculator(); -> calculator 클래스사용을 위한 새로운 객체를 생성합니다.
-        // 해당 객체를 통해 Calculator 클래스의 메서드를 사용할 수 있습니다.
-        // 이 코드가 없다면 Calculator 클래스의 메서드를 사용할 수 없습니다.
-        // 이러한 분리 작업으로 캡슐화를 통해 코드의 보안성을 높이고 재사용성을 높이기 위함입니다.
-        // 객체 내부 상태를 외부에서 직접 접근하지 못하도록 하기 위해 private로 선언된 필드에 접근할 수 있도록 getter와 setter를 이용한 간접 접근을 통해 필드에 접근할 수 있습니다.
-        // 필드란 클래스 내부에 선언된 변수를 의미합니다.
+        /*
+         * 해당 객체를 통해 Calculator 클래스의 메서드를 사용할 수 있습니다.
+         * 이 코드가 없다면 Calculator 클래스의 메서드를 사용할 수 없습니다.
+         * 이러한 분리 작업으로 캡슐화를 통해 코드의 보안성을 높이고 재사용성을 높이기 위함입니다.
+         * 객체 내부 상태를 외부에서 직접 접근하지 못하도록 하기 위해 private로 선언된 필드에 접근할 수 있도록 getter와 setter를 이용한 간접 접근을 통해 필드에 접근할 수 있습니다.
+         * 필드란 클래스 내부에 선언된 변수를 의미합니다.
+         */
         Scanner sc = new Scanner(System.in);
-        //  Scanner sc = new Scanner(System.in); -> scanner sc는 콘솔창에서 입력받기 위한 스캐너 ,
-        //  System.in은 키보드 입력을 의미 , new scanner은 새로운 스캐너 객체를 생성한다는 의미
+        /*
+         * Scanner sc = new Scanner(System.in); -> scanner sc는 콘솔창에서 입력받기 위한 스캐너 ,
+         * System.in은 키보드 입력을 의미 , new scanner은 새로운 스캐너 객체를 생성한다는 의미
+         */
 
         while (true) {
-            System.out.println("첫 번째 숫자를 입력하세요 : ");
-            int num1 = sc.nextInt();
 
-            System.out.println("두 번째 숫자를 입력하세요 : ");
-            int num2 = sc.nextInt();
+            // 사칙연산 외에 반복문에 사칙연산을 할지 원의 넓이를 구할지 선택지를 만들엊ㅜㅂ니다
+            System.out.println("원의 넓이를 구하려면 1번 , 사칙연산을 하려면 2번을 입력하세요 : ");
+            int select = sc.nextInt(); // select 변수에 스캐너로 입력받은 값을 저장
+            int choice = 0; // choice 변수를 0으로 초기화
 
-            System.out.println("사칙연산자를 입력하세요 : ");
-            char operator = sc.next().charAt(0); // charAt(0)을 통해 문자열의 첫번째 문자를 가져옴 , chrAt은 문자열의 인덱스를 가져오는 메서드
-
-            try {
-                int result = calculator.calculate(num1, num2, operator); // int result에 calculate 메서드를 통해 계산 결과 값을 저장
-                System.out.println("결과 : " + result); // calculate 메서드를 통해 계산 결과를 콘솔에 출력합니다.
-            } catch (ArithmeticException e) {
-                System.out.println("에러 발생 : " + e.getMessage()); // getMessage() 메서드를 통해 throw한 ArithmeticException 메세지를 가져와서 에러메세지를 출력!!
-            } catch (IllegalArgumentException e) {
-                System.out.println("에러 발생 : " + e.getMessage()); // getMessage() 메서드를 통해 throw한 IllegalArgumentException 메세지를 가져와서 에러메세지를 출력!!
+            if (select != 1 && select != 2) { // select가 1 또는 2가 아닌 경우 예외처리를 합니다.
+                System.out.println("1번과 2번 중에 하나만 입력해주세요!! "); // 1번과 2번 중에 선택하라는 메세지를 출력합니다.
+                continue; // continue를 사용하여 반복문을 다시 실행합니다.
             }
 
-            ArrayList<Integer> list = calculator.getList(); // getList 메서드로 배열을 가져옵니다 , 이렇게 한다면 다른 클래스에서도 list 배열을 가져올 수 있습니다.
-            System.out.println("리스트 배열에 저장된 값: " + list); // 리스트 배열에 저장된 값을 출력
-
-            System.out.println("더 계산하시겠습니까? 아무키나 입력해주세요 (exit 입력 시 종료 , inquiry 입력 시 조회 ,  remove 입력 시 맨 앞리스트만 삭제) : ");
-            String answer = sc.next(); //  스트링 answer에 스캐너로 입력받은 값을 저장
-            // 명령어가 아닌 아무키나 입력받는다면 계속해서 계산합니다
-            if (answer.equalsIgnoreCase("exit")) {// answer.equalsIgnoreCase("exit")로 입력받은 값이 exit인 경우 프로그램을 종료합니다,
-                // answer.equalsIgnoreCase()는 대소문자를 구분하지 않고 비교하고 싶을 때 사용합니다.
-                // 대소문자 비교가 필요없다면 equals...()를 사용하면 됩니다.
-                //answer은 스트링이기 때문에 equals...()를 사용하여 비교할 수 있습니다.
-                break;
-            } else if (answer.equalsIgnoreCase("inquiry")) { // answer.equalsIgnoreCase("inquiry")로 입력받은 값이 inquiry인 경우 리스트 배열에 저장된 값을 조회합니다.
-                calculator.inquiryList(); // inquiryList 메서드를 호출하여 리스트 배열에 저장된 값을 조회합니다.
-            } else if (answer.equalsIgnoreCase("remove")) { // answer.equalsIgnoreCase("remove")로 입력받은 값이 remove인 경우 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
-                calculator.removeFirstList(); // removeFirstList 메서드를 호출하여 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
+            if (select == 1) { // select가 1인 경우 원의 넓이를 구하는 메서드를 실행합니다.
+                choice = 1; // choice 변수에 1을 저장
+                System.out.println("반지름을 입력하세요 : ");
+                int radius = sc.nextInt(); // radius 변수에 스캐너로 입력받은 값을 저장
+                double result = calculator.calculateCircleArea(radius); // calculateCircleArea 메서드를 통해 원의 넓이를 계산합니다.
+                System.out.println("결과 : " + result); // calculateCircleArea 메서드를 통해 계산 결과를 콘솔에 출력합니다.
             }
+
+            if (select == 2) { // select가 2인 경우 사칙연산을 실행합니다.
+                choice = 2; // choice 변수에 2를 저장
+                System.out.println("사칙연산을 시작합니다.");
+
+                System.out.println("첫 번째 숫자를 입력하세요 : ");
+                int num1 = sc.nextInt();
+
+                System.out.println("두 번째 숫자를 입력하세요 : ");
+                int num2 = sc.nextInt();
+
+                System.out.println("사칙연산자를 입력하세요 : ");
+                char operator = sc.next().charAt(0); // charAt(0)을 통해 문자열의 첫번째 문자를 가져옴 , chrAt은 문자열의 인덱스를 가져오는 메서드
+
+                try {
+                    int result = calculator.calculate(num1, num2, operator); // int result에 calculate 메서드를 통해 계산 결과 값을 저장
+                    System.out.println("결과 : " + result); // calculate 메서드를 통해 계산 결과를 콘솔에 출력합니다.
+                } catch (ArithmeticException e) {
+                    System.out.println("에러 발생 : " + e.getMessage()); // getMessage() 메서드를 통해 throw한 ArithmeticException 메세지를 가져와서 에러메세지를 출력!!
+                } catch (IllegalArgumentException e) {
+                    System.out.println("에러 발생 : " + e.getMessage()); // getMessage() 메서드를 통해 throw한 IllegalArgumentException 메세지를 가져와서 에러메세지를 출력!!
                 }
-        sc.close(); // 스캐너 종료가 맨 마지막에 있는 이유는 스캐너를 사용하고 난 뒤에는 반드시 닫아주어야 하기 때문입니다.
-        // 닫아주지 않는다면 메모리 누수가 발생할 수 있습니다. 이런 방식의 메모리 누수는 프로그램이 종료될 때까지 계속해서 누적되어 쌓이게 됩니다.
-        // 즉, 루트 밖에 마지막에 스캐너 종료가 맨 마지막에 있는 이유는 스캐너를 사용 한 후 리소스를 반환하여 메모리 누수를 방지하기 위함입니다.
-    }
-}
+
+            } // if문 종료
+
+            if (choice == 1) { // choice 가 1인 경우 원의 넓이와 관련된 배열이나 명령어를 실행합니다.
+                ArrayList<Double> list = calculator.getCircleArea(); // getCircleArea 메서드로 배열을 가져옵니다 , 이렇게 한다면 다른 클래스에서도 list 배열을 가져올 수 있습니다.
+                System.out.println("리스트 배열에 저장된 값: " + list); // 리스트 배열에 저장된 값을 출력
+
+                System.out.println("더 계산하시겠습니까? 아무키나 입력해주세요 (exit 입력 시 종료 , inquiry 입력 시 조회 ,  remove 입력 시 맨 앞리스트만 삭제) : ");
+                String answer = sc.next(); //  스트링 answer에 스캐너로 입력받은 값을 저장
+                // 명령어가 아닌 아무키나 입력받는다면 계속해서 계산합니다
+
+                if (answer.equalsIgnoreCase("exit")) {// answer.equalsIgnoreCase("exit")로 입력받은 값이 exit인 경우 프로그램을 종료합니다,
+                    break;
+                } else if (answer.equalsIgnoreCase("inquiry")) { // answer.equalsIgnoreCase("inquiry")로 입력받은 값이 inquiry인 경우 리스트 배열에 저장된 값을 조회합니다.
+                    calculator.inquiryCircleArea(); // inquiryCircleArea 메서드를 호출하여 리스트 배열에 저장된 값을 조회합니다.
+                } else if (answer.equalsIgnoreCase("remove")) { // answer.equalsIgnoreCase("remove")로 입력받은 값이 remove인 경우 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
+                    calculator.removeFirstCircleArea(); // removeFirstCircleArea 메서드를 호출하여 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
+                }
+            } // if문 종료
+
+            if (choice == 2) { // choice 가 2인 경우 사칙연산과 관련된 배열이나 명령어를 실행합니다.
+                ArrayList<Integer> list = calculator.getList(); // getList 메서드로 배열을 가져옵니다 , 이렇게 한다면 다른 클래스에서도 list 배열을 가져올 수 있습니다.
+                System.out.println("리스트 배열에 저장된 값: " + list); // 리스트 배열에 저장된 값을 출력
+
+                System.out.println("더 계산하시겠습니까? 아무키나 입력해주세요 (exit 입력 시 종료 , inquiry 입력 시 조회 ,  remove 입력 시 맨 앞리스트만 삭제) : ");
+                String answer = sc.next(); //  스트링 answer에 스캐너로 입력받은 값을 저장
+                // 명령어가 아닌 아무키나 입력받는다면 계속해서 계산합니다
+
+                if (answer.equalsIgnoreCase("exit")) {// answer.equalsIgnoreCase("exit")로 입력받은 값이 exit인 경우 프로그램을 종료합니다,
+                    /*
+                     * answer.equalsIgnoreCase()는 대소문자를 구분하지 않고 비교하고 싶을 때 사용합니다.
+                     * 대소문자 비교가 필요없다면 equals...()를 사용하면 됩니다.
+                     *answer은 스트링이기 때문에 equals...()를 사용하여 비교할 수 있습니다.
+                     */
+                    break;
+                } else if (answer.equalsIgnoreCase("inquiry")) { // answer.equalsIgnoreCase("inquiry")로 입력받은 값이 inquiry인 경우 리스트 배열에 저장된 값을 조회합니다.
+                    calculator.inquiryList(); // inquiryList 메서드를 호출하여 리스트 배열에 저장된 값을 조회합니다.
+                } else if (answer.equalsIgnoreCase("remove")) { // answer.equalsIgnoreCase("remove")로 입력받은 값이 remove인 경우 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
+                    calculator.removeFirstList(); // removeFirstList 메서드를 호출하여 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
+                }
+            } // if문 종료
+
+        } // while문 종료
+
+        sc.close(); // 스캐너 종료
+        /*
+         * 스캐너 종료가 맨 마지막에 있는 이유는 스캐너를 사용하고 난 뒤에는 반드시 닫아주어야 하기 때문입니다.
+         * 닫아주지 않는다면 메모리 누수가 발생할 수 있습니다. 이런 방식의 메모리 누수는 프로그램이 종료될 때까지 계속해서 누적되어 쌓이게 됩니다.
+         *  즉, 루트 밖에 마지막에 스캐너 종료가 맨 마지막에 있는 이유는 스캐너를 사용 한 후 리소스를 반환하여 메모리 누수를 방지하기 위함입니다.
+         */
+    } //  main 메서드 종료 ( 괄호 위치 확인용 주석 )
+} // App 클래스 종료 ( 괄호 위치 확인용 주석 )
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class Calculator { // Calculator 클래스를 생성
 
-    private ArrayList<Integer> list; // private로 선언된 리스트 배열을 생성합니다.
-    /* 생성자가 있기 때문에 static을 사용하지 않아도 됩니다.
+    private static final double PI = 3.14;
+    // 원주율 상수 선언, 상수는 변하지 않는 값을 의미며 final로 선언하여 값을 변경할 수 없게 만들어 신뢰할 수 있는 값을 만듭니다.
+    private ArrayList<Integer> list; // 사칙연산 저장
+    /*
+    * 생성자가 있기 때문에 static을 사용하지 않아도 됩니다.
     * static은 클래스 변수로 클래스가 로딩될 때 메모리에 할당되고 프로그램이 종료될 때까지 메모리에 남아있는 변수를 사용합니다.
     * 이전에는 static을 사용하여 메모리에 할당되어 프로그램이 종료될 때까지 메모리에 남아있는 변수를 사용했지만,
     * 이번에는 생성자를 사용하여 객체를 생성하여 메모리에 할당하고 프로그램이 종료될 때 메모리에서 해제되는 방식을 사용합니다.
@@ -13,6 +16,8 @@ public class Calculator { // Calculator 클래스를 생성
     *  private ArrayList<Integer> list; = private로 선언하여 다른 클래스에서 접근하지 못하고 getter와 setter를 사용하여 간접 접근이 가능한 list 배열을 만듭니다.
     * (요약 : static은 종료시에 메모리에 남지만 생성자는 종료시에 메모리에서 해제됩니다.)
     */
+    private ArrayList<Double> circleArea; // 원주율 저장
+
     public Calculator() { // 생성자 생성.
         this.list = new ArrayList<>(); // 생성자를 통해 리스트 배열을 초기화합니다.
         /* 생성자 = 클래스명과 돌일한 이름을 가지고 있는 객체이며 생성 시 호출되는 메서드로 객체를 초기화하는 역할을 합니다.
@@ -23,14 +28,18 @@ public class Calculator { // Calculator 클래스를 생성
         * 이 생성자가 없다면 예시 : app에서 사용 시 list.add(result)를 사용하여 리스트 배열에 저장된 연산 결과 값을 가져올 수 없습니다.
         * 시도할 경우 에러가 발생합니다.
         */
-
+        this.circleArea = new ArrayList<>(); // 생성자를 통해 원주율 배열을 초기화합니다.
     }
 
     // 사칙연산 메서드
-    public int calculate(int num1, int num2, char operator) { // 반환 타입은 int로 설정하고 매개변수로 num1, num2, operator(연산자)를 받습니다.
+    public int calculate(int num1, int num2, char operator) {
+        /*
+        // 반환 타입은 int로 설정하고 매개변수로 num1, num2, operator(연산자)를 받습니다.
         // private로 선언하는 이유는 다른 클래스에서 접근하지 못하도록 하기 위함입니다.
         // 다른 클래스에서 접근하지 못하도록 한다면 getter와 setter를 사용하여 간접 접근을 통해 필드에 접근할 수 있도록 구현할 수 있습니다.
         // 그래서하단에 getter와 setter를 추가해줍니다.
+        */
+
         int result = 0; // 연산 후 값을 저장할 변수
         // 요구사항에 맞게 throw로 예외처리를 해주기위해 error 변수는 필요없어요
 
@@ -57,6 +66,62 @@ public class Calculator { // Calculator 클래스를 생성
         return result; // 연산 결과를 반환합니다.
     }
 
+    // 원의 넓이를 계산 후 반환해주는 메서드
+    public double calculateCircleArea(double radius) {
+        /* 반환 타입은 double로 설정하고 매개변수로 radius(반지름)를 받습니다.
+        * static으로 선언하는 이유는 객체를 생성하지 않고도 사용할 수 있도록 하기 위함입니다.
+        * private로 선언하는 이유는 다른 클래스에서 접근하지 못하도록 하기 위함입니다.
+        * 다른 클래스에서 접근하지 못하도록 한다면 getter와 setter를 사용하여 간접 접근을 통해 필드에 접근할 수 있도록 구현할 수 있습니다.
+        * 그래서하단에 getter와 setter를 추가해줍니다.
+        */
+        double area = PI * radius * radius; // 원의 넓이를 계산합니다. result를 적으면 위와 중복되니 변경해줍니다 area로 지은 이유는 원의 넓이를 의미???해서입니다
+        circleArea.add(area); // 원의 넓이를 원주율 배열에 저장합니다.
+        return area; // 원의 넓이를 반환합니다.
+    }
+
+    // 원주율 저장 필드 및 조회 메서드
+    public ArrayList<Double> getCircleArea() {
+        // 요구사항을 만족하기 간접 접근을 통해 필드에 접근하여 가져 올 수 있도록 구현하기 위해서 getter를 사용하여 원주율 배열을 가져옴
+        // public ArrayList<Double> getCircleArea() { // 반환 타입 : ArrayList<Double> , getCircleArea = 원주율 배열을 가져옴 , 반환 값 : circleArea 배열
+        //app에서 사용 시 list.add(result)를 사용하여 리스트 배열에 저장된 연산 결과 값을 가져올 수 있습니다.
+        return circleArea; // 원주율 배열을 반환합니다.
+    }
+
+    public void setCircleArea(ArrayList<Double> circleArea) {
+        // 요구사항을 만족하기 간접 접근을 통해 필드에 접근하여 변경할 수 있도록 구현하기 위해서 setter를 사용하여 원주율 배열을 변경
+        // public void setCircleArea(ArrayList<Double> circleArea) { // 반환 타입 : void , setCircleArea = 원주율 배열을 변경 , 매개변수 : circleArea 배열
+        // 반환 값 : 없음 , 반환값이 없는 이유는 원주율 배열을 변경하기만 하기 때문입니다.
+        this.circleArea = circleArea; // 원주율 배열을 변경합니다.
+        // app에서 사용시 list.remove(0)을 사용하여 리스트 배열에 저장된 0,1,2,3과같은 수를 입력하여 인덱스 값에 따라 삭제할 수 있습니다.
+    }
+    public void inquiryCircleArea() { // 원주율 배열에 저장된 값을 조회하는 메서드를 추가
+        // public void inquiryCircleArea() { // 반환 타입 : void , inquiryCircleArea = 원주율 배열에 저장된 값을 조회 , 매개변수 : 없음 ,
+        // 반환 값 : 없음 , 반환값과 매개변수가 없는 이유는 원주율 배열에 저장된 값을 조회하기만 하기 때문입니다.
+        if (!circleArea.isEmpty()) { // 조건 : 원주율 배열에 저장된 값이 없는 경우가 아닌 경우, !가 부정을 의미하기 떄문에 원주율 배열에 저장된 값이 있는 경우입니다.
+            // isEmpty()는 원주율 배열이 비어있는지 확인하는 메서드 엠티 = 비어있다 = !부정.원주율.비어있다 원주율이 비어있는지에 대한 조건
+            System.out.println("원주율 배열에 저장된 값 : " + circleArea); // 원주율 배열에 저장된 값을 출력합니다.
+        }
+        else {
+            System.out.println("원주율 배열에 저장된 값이 없습니다!!!"); // 원주율 배열에 저장된 값이 없는 경우 if 에서 !가 없는경우와 같습니다.
+        }
+    }
+
+    public void removeFirstCircleArea() { // 원주율 배열의 맨 앞의 값을 삭제하는 메서드를 추가
+        // public void removeFirstCircleArea() { // 반환 타입 : void , removeFirstCircleArea = 원주율 배열의 맨 앞의 값을 삭제 , 매개변수 : 없음 ,
+        // 반환 값 : 없음 , 반환값과 매개변수가 없는 이유는 원주율 배열의 맨 앞의 값을 삭제하기만 하기 때문입니다.
+        if (!circleArea.isEmpty()) { // 조건 : 원주율 배열에 저장된 값이 없는 경우가 아닌 경우, !가 부정을 의미하기 떄문에 원주율 배열에 저장된 값이 있는 경우입니다.
+            // isEmpty()는 원주율 배열이 비어있는지 확인하는 메서드 엠티 = 비어있다 = !부정.원주율.비어있다 원주율이 비어있는지에 대한 조건
+            System.out.println("원주율 배열의 맨 앞의 값이 삭제되었습니다. 삭제된 값 : " + circleArea.get(0)); // 원주율 배열의 맨 앞의 값이 삭제된 경우
+            circleArea.remove(0); // 원주율 배열에 저장된 맨 앞의 값을 삭제합니다.
+            // 만약 원주율 맨 앞의값이 아닌 다른값을 삭제시키고 싶다면 circleArea.remove(0)을 circleArea.remove(1)로 변경
+            // 즉, 안에 숫자는 인덱스를 의미합니다.
+            System.out.println("현재 원주율 배열에 저장된 값 : " + circleArea); // 원주율 배열에 저장된 값을 출력합니다.
+        }
+        else {
+            System.out.println("원주율 배열에 저장된 값이 없습니다!!!"); // 원주율 배열에 저장된 값이 없는 경우
+        }
+    }
+
     // 요구사항을 구현하기 app 클래스의 메인에서 사용할수있는 리스트 맨 앞의 값을 삭제하는 메서드를 추가
     public void removeFirstList() { //
         // public void public void removeFirstList() { //() { // 반환 타입 : void ,     public void removeFirstList() { // = 리스트 배열의 맨 앞의 값을 삭제 , 매개변수 : 없음 ,
@@ -69,7 +134,7 @@ public class Calculator { // Calculator 클래스를 생성
             list.remove(0); // 리스트 배열에 저장된 맨 앞의 값을 삭제합니다.
             // 만약 리스트 맨 앞의값이 아닌 다른값을 삭제시키고 싶다면 list.remove(0)을 list.remove(1)로 변경
             // 즉, 안에 숫자는 인덱스를 의미합니다.
-            System.out.println("현재 리스트 배열에 저장된 값: " + list); // 리스트 배열에 저장된 값을 출력합니다.
+            System.out.println("현재 리스트 배열에 저장된 값 : " + list); // 리스트 배열에 저장된 값을 출력합니다.
         }
         else {
             System.out.println("리스트 배열에 저장된 값이 없습니다!!!"); // 리스트 배열에 저장된 값이 없는 경우
@@ -81,7 +146,7 @@ public class Calculator { // Calculator 클래스를 생성
         // 반환 값 : 없음 , 반환값과 매개변수가 없는 이유는 리스트 배열에 저장된 값을 조회하기만 하기 때문입니다.
         if (!list.isEmpty()) { // 조건 : 리스트 배열에 저장된 값이 없는 경우가 아닌 경우, !가 부정을 의미하기 떄문에 리스트 배열에 저장된 값이 있는 경우입니다.
             // isEmpty()는 리스트 배열이 비어있는지 확인하는 메서드 엠티 = 비어있다 = !부정.리스트.비어있다 리스트가 비어있는지에 대한 조건
-            System.out.println("리스트 배열에 저장된 값: " + list); // 리스트 배열에 저장된 값을 출력합니다.
+            System.out.println("리스트 배열에 저장된 값 : " + list); // 리스트 배열에 저장된 값을 출력합니다.
         }
         else {
             System.out.println("리스트 배열에 저장된 값이 없습니다!!!"); // 리스트 배열에 저장된 값이 없는 경우
@@ -96,17 +161,16 @@ public class Calculator { // Calculator 클래스를 생성
     }
 
     public void setList (ArrayList<Integer> list) {
+        /*
         // 요구사항을 만족하기 간접 접근을 통해 필드에 접근하여 변경할 수 있도록 구현하기 위해서 setter를 사용하여 리스트 배열을 변경
         // public void setList(ArrayList<Integer> list) { // 반환 타입 : void , setList = 리스트 배열을 변경 , 매개변수 : list 배열
         // 반환 값 : 없음 번환 값이 없는 이유는 리스트 배열을 변경하기만 하기 때문입니다.
         // integer = 정수 데이터를 저장하는 변수 타입 int와 차이점은 int는 기본 데이터 타입이고 integer는 참조 데이터 타입입니다.
         //참조 데이터는 객체를 참조하는 데이터 타입이고 객체를 참조하는 데이터 타입이기 때문에 객체를 생성해야 사용할 수 있습니다.
         // 즉 , int는 객체를 생성하지 않아도 사용할 수 있지만 integer는 객체를 생성해야 사용할 수 있습니다.
+        */
+
         this.list = list; // 리스트 배열을 변경합니다.
         // app에서 사용시 list.remove(0)을 사용하여 리스트 배열에 저장된 0,1,2,3과같은 수를 입력하여 인덱스 값에 따라 삭제할 수 있습니다.
     }
-
-// 배열을 변경하고 적용하기위해 removeFirstList()를 먼저 호출하고 setList()를 호출하여 변경된 리스트 배열을 가져옵니다.
-
 }
-
