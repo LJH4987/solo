e7f57df73a89510e10801c24bf64f0f1ef62189f
Robbie <robbie331@>
2024-07-31 01:11:43 +0900

    Level 2-9 "사칙연산 메소드의 책임분리"

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,17 +1,29 @@
-// 해당 클래스는 계산기의 기본적인 기능을 제공하는 클래스입니다.
-// 기능 = 리스트 배열에 값을 저장하는 기능(setList),
-// 리스트 배열의 맨 앞의 값을 삭제하는 기능(removeFirstList),
-// 리스트 배열에 저장된 값을 조회하는 기능(inquiryList)을 담당합니다.
+// SPR을 통해 책임을 분리시킨 클래스들을 상속받아 사칙연산을 수행하는 클래스
 
 package calculator;
 
 import java.util.ArrayList;
 
 public class Calculator {
-    protected ArrayList<Double> list;
+    protected ArrayList<Double> list; // 사칙연산 결과를 저장하는 리스트
+
+    protected AddOperator addOperator;
+    protected SubtractOperator subtractOperator;
+    protected MultiplyOperator multiplyOperator;
+    protected DivideOperator divideOperator;
+
+    // protected 접근 제어자를 사용하여 새로 생성한
+    // AddOperator, SubtractOperator, MultiplyOperator, DivideOperator 클래스를 상속받는 클래스에서 접근할 수 있도록 합니다.
 
     public Calculator() {
         this.list = new ArrayList<>();
+        this.addOperator = new AddOperator();
+        this.subtractOperator = new SubtractOperator();
+        this.multiplyOperator = new MultiplyOperator();
+        this.divideOperator = new DivideOperator();
+
+        // AddOperator, SubtractOperator, MultiplyOperator, DivideOperator 클래스의 인스턴스를 생성합니다.
+
     }
 
     public ArrayList<Double> getList() {
@@ -22,19 +34,19 @@ public class Calculator {
         this.list = list;
     }
 
-    public void removeFirstList() {
+    public void inquiryList() {
         if (!list.isEmpty()) {
-            System.out.println("리스트 배열의 맨 앞의 값이 삭제되었습니다. 삭제된 값 : " + list.get(0));
-            list.remove(0);
-            System.out.println("현재 리스트 배열에 저장된 값 : " + list);
+            System.out.println("리스트 배열에 저장된 값 : " + list);
         } else {
             System.out.println("리스트 배열에 저장된 값이 없습니다!!!");
         }
     }
 
-    public void inquiryList() {
+    public void removeFirstList() {
         if (!list.isEmpty()) {
-            System.out.println("리스트 배열에 저장된 값 : " + list);
+            System.out.println("리스트 배열의 맨 앞의 값이 삭제되었습니다. 삭제된 값 : " + list.get(0));
+            list.remove(0);
+            System.out.println("현재 리스트 배열에 저장된 값 : " + list);
         } else {
             System.out.println("리스트 배열에 저장된 값이 없습니다!!!");
         }
